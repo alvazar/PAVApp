e36b8027707a26d3126a11b629abe604b69be46e
alvazar <alvazar@gmail.com>
2021-02-14 14:19:45 +0300

    update

--- src/PAVApp/MVC/RouteInterface.php
+++ src/PAVApp/MVC/RouteInterface.php
@@ -6,7 +6,7 @@ use PAVApp\Core\RequestInterface;
 
 interface RouteInterface
 {
-    public static function run(string $cb, array $params = []): ResultInterface;
+    public static function run(mixed $cb, array $params = []): ResultInterface;
     public static function get(string $queryTrigger, string $cb, array $params = []): void;
     public static function post(string $queryTrigger, string $cb, array $params = []): void;
     public static function start(RequestInterface $Req): void;
